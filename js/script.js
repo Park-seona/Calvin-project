28b1533ce54a7803852ee6cde5dc72ce228c448b
Park-seona <brown484@naver.com>
2023-04-05 19:48:43 +0900

    0405

---
+++ js/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+
+    //네비게이션 메뉴
+    $("header .nav_box ul.main>li").mouseenter(function(){
+        $(this).find("ul.sub_menu").stop().fadeIn();
+    });
+    $("header .nav_box ul.main>li").mouseleave(function(){
+        $(this).find("ul.sub_menu").stop().fadeOut();
+    });
+    
+    //뉴배너 스와이퍼
+    const swiper = new Swiper('.swiper', {
+        slidesPerView: 3, // 한 화면에 보일 슬라이드 개수
+        spaceBetween: 20, // 슬라이드 간의 간격 설정
+      
+        // 화살표 필요 없으니 제거
+        // navigation: {
+        //   nextEl: '.swiper-button-next',
+        //   prevEl: '.swiper-button-prev',
+        // },
+      
+        // 스크롤바 필요 !
+        scrollbar: {
+          el: '.swiper-scrollbar',
+          draggable: true, // 지우면 `false` 기본값, 스크롤바 드래그해서 이동 가능
+        },
+    });
+    
+    //베스트 배너 아이콘 애니메이션
+    $(".contents .best_banner .best_nav .item2,.contents .best_banner .best_nav .item2 img").mouseenter(function(){
+        $(".contents .best_banner .best_nav .item2 .heart").stop().animate({"top":"5px"},280);
+    });
+    $(".contents .best_banner .best_nav .item2,.contents .best_banner .best_nav .item2 img").mouseleave(function(){
+        $(".contents .best_banner .best_nav .item2 .heart").stop().animate({"top":"0"},280);
+    });
+    //컬렉션 배너
+    $(".contents .item1_wrapper .item1 img,.item1_wrapper .item1_box").mouseenter(function(){
+        $(".contents .collection_banner .item1_wrapper .item1_box").stop().animate({"bottom":"15px"},280);
+    });
+    $(".contents .item1_wrapper .item1 img,.item1_wrapper .item1_box").mouseleave(function(){
+        $(".contents .collection_banner .item1_wrapper .item1_box").stop().animate({"bottom":"-50px"},280);
+    });
+    $(".contents .item2_wrapper .item2 img,.item2_wrapper .item2_box").mouseenter(function(){
+        $(".contents .collection_banner .item2_wrapper .item2_box").stop().animate({"bottom":"15px"},280);
+    });
+    $(".contents .item2_wrapper .item2 img,.item2_wrapper .item2_box").mouseleave(function(){
+        $(".contents .collection_banner .item2_wrapper .item2_box").stop().animate({"bottom":"-50px"},280);
+    });
+    
+});
